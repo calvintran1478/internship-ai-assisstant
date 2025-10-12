4ea7c6848db009ce20efac494fbbf1a99d4f8093
Calvin Tran <calvintran1478@protonmail.com>
2025-10-11 22:34:49 -0400

    Initialize client directory

---
+++ client/internship-ai-assisstant/src/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+    return (
+        <div class="flex flex-col justify-center items-center h-screen w-screen">
+            <label class="text-3xl mb-8">Hello! How can I help with your career?</label>
+            <input class="border rounded w-lg h-10" placeholder="Ask me anything..."></input>
+        </div>
+    )
+}
+
+export default App
