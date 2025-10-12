d5b5a33eba6d8171c27e5323aeb94d0c6833e9c5
Calvin Tran <calvintran1478@protonmail.com>
2025-10-11 23:49:24 -0400

    Add functionality for sending prompts to the LLM and presenting the output

--- client/internship-ai-assisstant/src/App.tsx
+++ client/internship-ai-assisstant/src/App.tsx
@@ -1,10 +1,28 @@
+import { useState } from "react"
 
 function App() {
+    const [data, setData] = useState("");
+    const [prompt, setPrompt] = useState("");
+
+    const sendRequest = async (event: any) => {
+        event.preventDefault()
+        const response = await fetch("http://localhost:8000/api/v1/test", {
+            method: "POST",
+            body: prompt
+        });
+
+        if (response.ok) {
+            setData(await response.text());
+        }
+    }
 
     return (
-        <div class="flex flex-col justify-center items-center h-screen w-screen">
-            <label class="text-3xl mb-8">Hello! How can I help with your career?</label>
-            <input class="border rounded w-lg h-10" placeholder="Ask me anything..."></input>
+        <div className="flex flex-col justify-center items-center h-screen w-screen">
+            <form className="flex flex-col" onSubmit={sendRequest}>
+                <label className="text-3xl mb-8" >Hello! How can I help with your career?</label>
+                <input className="border rounded w-lg h-10" onChange={(e) => setPrompt(e.target.value)} placeholder="Ask me anything..."></input>
+            </form>
+            <p>{data}</p>
         </div>
     )
 }
