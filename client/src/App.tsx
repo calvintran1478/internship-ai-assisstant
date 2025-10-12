85b250317574c97ba46ec487351a5cf0b2eb23e0
Calvin Tran <calvintran1478@protonmail.com>
2025-10-12 19:47:27 -0400

    Add basic chat functionality to frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,47 @@
-import { createSignal } from 'solid-js'
+import { createSignal, Show, For } from 'solid-js'
 
 function App() {
-    const [data, setData] = createSignal("");
+    const [chat, setChat] = createSignal([] as string[]);
     let prompt = "";
 
     const sendRequest = async (event: Event) => {
         event.preventDefault();
+        const updatedChat = [...chat()];
+        updatedChat.push(prompt);
+
         const response = await fetch("http://localhost:8000/api/v1/chat", {
             method: "POST",
             body: prompt
         });
 
         if (response.ok) {
-            setData(await response.text());
+            updatedChat.push(await response.text());
+            setChat(updatedChat);
         }
     }
 
     return (
-        <div class="flex flex-col justify-center items-center h-screen w-screen">
-            <form class="flex flex-col" onSubmit={sendRequest}>
-                <label class="text-3xl mb-8">Hello! How can I help with your career?</label>
-                <input class="border rounded w-lg h-10" onChange={(event) => {prompt = event.target.value}} placeholder="Ask me anything..."/>
-            </form>
-            <p>{data()}</p>
+        <div>
+            <Show when={chat().length === 0}>
+                <div class="flex flex-col justify-center items-center h-screen w-screen">
+                    <form class="flex flex-col" onSubmit={sendRequest}>
+                        <label class="text-3xl mb-8">Hello! How can I help with your career?</label>
+                        <input class="border rounded w-lg h-10" onChange={(event) => {prompt = event.target.value}} placeholder="Ask me anything..."/>
+                    </form>
+                </div>
+            </Show>
+            <Show when={chat().length !== 0}>
+                <div class="flex flex-col items-center">
+                    <For each={chat()}>
+                        {(chatMessage, index) => (
+                            <p style={`top: ${index() * 80}px`} class={`fixed border rounded p-4 m-4 ${index() % 2 === 0 ? "right-0" : "left-0"}`}>{chatMessage}</p>
+                        )}
+                    </For>
+                    <form class="flex flex-col fixed bottom-20" onSubmit={sendRequest}>
+                        <input class="border rounded w-lg h-10" onChange={(event) => {prompt = event.target.value}} placeholder="Ask me anything..."/>
+                    </form>
+                </div>
+            </Show>
         </div>
     )
 }
