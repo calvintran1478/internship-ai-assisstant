8b543a7f48ebf0e86cbbbb1cd67aaf7e1ebdb315
Calvin Tran <calvintran1478@protonmail.com>
2025-10-13 18:53:36 -0400

    Add python class representing streamed data coming from the LLM

--- server/src/resources/chat.py
+++ server/src/resources/chat.py
@@ -1,8 +1,9 @@
 import falcon
+from llm.llm_stream import LLMStream
 
 class ChatResource:
     async def on_post(self, req, resp):
         body = await req.get_media()
 
         resp.status = falcon.HTTP_201
-        resp.text = body
+        resp.stream = LLMStream(body)
