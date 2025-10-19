4ecec56ba8c5e481f17de87aed5f15557e1d78a1
Calvin Tran <calvintran1478@protonmail.com>
2025-10-18 22:16:45 -0400

    Validate request body on register endpoint

--- server/src/resources/user.py
+++ server/src/resources/user.py
@@ -1,15 +1,27 @@
 import falcon
 import asyncpg
+from email_validator import validate_email, EmailNotValidError
 from bcrypt import hashpw, gensalt
 from uuid import uuid4
 
 class UserResource:
     async def on_post(self, req, resp):
         # Parse request body
-        body = await req.get_media()
-        email, password, first_name, last_name = body.split("\n")
+        email, password, first_name, last_name = (await req.get_media()).split("\n")
+        if email == "" or password == "" or first_name == "" or last_name == "":
+            resp.status = falcon.HTTP_400
+            resp.text = "Missing fields in request body"
+            return
 
-        # Check if user with email already exists
+        # Check that the email is valid and normalize it
+        try:
+            email = validate_email(email, check_deliverability=False).normalized
+        except EmailNotValidError:
+            resp.status = falcon.HTTP_400
+            resp.text = "Invalid email address"
+            return
+
+        # Check if a user with the email already exists
         statement = await req.context.conn.prepare("SELECT EXISTS(SELECT 1 FROM users WHERE email=$1)")
         user_exists = await statement.fetchval(email)
         if user_exists:
@@ -17,7 +29,7 @@ class UserResource:
             resp.text = "User with email already exists"
             return
 
-        # Hash password and register user in database
+        # Hash password and register user in the database
         hashed_password = hashpw(password.encode("utf-8"), gensalt())
         statement = await req.context.conn.prepare("INSERT INTO users (user_id, email, password, first_name, last_name) VALUES ($1, $2, $3, $4, $5)")
         await statement.fetchval(uuid4(), email, password, first_name, last_name)
