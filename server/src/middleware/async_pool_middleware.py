e8a28d12c870cd3116ba30e961597219fadcc1a7
Calvin Tran <calvintran1478@protonmail.com>
2025-10-18 21:54:43 -0400

    Initialize database tables on start up

--- server/src/middleware/async_pool_middleware.py
+++ server/src/middleware/async_pool_middleware.py
@@ -6,7 +6,20 @@ class AsyncPoolMiddleware:
         self.pool = None
 
     async def process_startup(self, scope, event):
+        # Create connection pool
         self.pool = await asyncpg.create_pool(database="internship_ai_db", user="internship_ai_user")
+
+        # Initialize database tables if needed
+        async with self.pool.acquire() as conn:
+            await conn.execute("""
+                CREATE TABLE IF NOT EXISTS users(
+                    user_id UUID PRIMARY KEY,
+                    email VARCHAR UNIQUE NOT NULL,
+                    password VARCHAR NOT NULL,
+                    first_name VARCHAR NOT NULL,
+                    last_name VARCHAR NOT NULL
+                );
+                """)
 
     async def process_shutdown(self, scope, event):
         await asyncio.wait_for(self.pool.close(), 1.0)
