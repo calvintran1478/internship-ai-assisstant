a525ad215bc6de59b9039b79523db0341812f96b
Calvin Tran <calvintran1478@protonmail.com>
2025-10-18 21:00:04 -0400

    Implement basic register endpoint

--- server/src/server.py
+++ server/src/server.py
@@ -1,6 +1,7 @@
 import falcon
 import falcon.asgi
 from resources.chat import ChatResource
+from resources.user import UserResource
 from handlers.text_handler import TextHandler
 
 # Initialize server with default plain/text content type
@@ -12,5 +13,7 @@ app.req_options.media_handlers['text/plain'] = text_handler
 app.resp_options.media_handlers['text/plain'] = text_handler
 
 # Set up server routes for each resource
+user_resource = UserResource()
 chat_resource = ChatResource()
+app.add_route("/api/v1/users", user_resource)
 app.add_route("/api/v1/chat", chat_resource)
