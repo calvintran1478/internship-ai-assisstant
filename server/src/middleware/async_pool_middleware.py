88ffb50a52f523531b3cbe9408061a8763712f01
Calvin Tran <calvintran1478@protonmail.com>
2025-10-31 17:20:48 -0400

    Set up pgvector

--- server/src/middleware/async_pool_middleware.py
+++ server/src/middleware/async_pool_middleware.py
@@ -1,16 +1,27 @@
+import os
 import asyncpg
 import asyncio
+from pgvector.asyncpg import register_vector
 
 class AsyncPoolMiddleware:
     def __init__(self):
         self.pool = None
 
+    async def initialize_vector_db(self, conn):
+        await register_vector(conn)
+
     async def process_startup(self, scope, event):
+        # Enable pgvector extension
+        conn = await asyncpg.connect(database=os.getenv("DB_NAME"), user=os.getenv("DB_USER"))
+        await conn.execute("CREATE EXTENSION IF NOT EXISTS vector")
+        await conn.close()
+
         # Create connection pool
-        self.pool = await asyncpg.create_pool(database="internship_ai_db", user="internship_ai_user")
+        self.pool = await asyncpg.create_pool(database=os.getenv("DB_NAME"), user=os.getenv("DB_USER"), init=self.initialize_vector_db)
 
         # Initialize database tables if needed
         async with self.pool.acquire() as conn:
+            # Create user table
             await conn.execute("""
                 CREATE TABLE IF NOT EXISTS users(
                     user_id UUID PRIMARY KEY,
@@ -18,6 +29,17 @@ class AsyncPoolMiddleware:
                     password VARCHAR NOT NULL,
                     first_name VARCHAR NOT NULL,
                     last_name VARCHAR NOT NULL
+                );
+                """)
+
+            # Create resume table
+            await conn.execute("""
+                CREATE TABLE IF NOT EXISTS resumes(
+                    user_id UUID PRIMARY KEY,
+                    embedding vector(3),
+                    CONSTRAINT resumes_user_id_fkey FOREIGN KEY(user_id) REFERENCES users(user_id)
+                        ON DELETE CASCADE
+                        ON UPDATE CASCADE
                 );
                 """)
 
