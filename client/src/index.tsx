423a25fdabb63928117b283fa1d1ca0e05d8c1ae
Calvin Tran <calvintran1478@protonmail.com>
2025-10-31 12:32:02 -0400

    Add missing frontend routes

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,13 +3,15 @@ import { render } from "solid-js/web";
 import { Router, Route } from "@solidjs/router"; 
 import "./index.css"
 
-const HomePage = lazy(() => import("./pages/HomePage"));
 const RegisterPage = lazy(() => import("./pages/RegisterPage"));
+const LoginPage = lazy(() => import("./pages/LoginPage"));
+const HomePage = lazy(() => import("./pages/HomePage"));
 
 render(
     () => (
         <Router root={(props) => <>{props.children}</>}>
             <Route path="/register" component={RegisterPage}/>
+            <Route path="/login" component={LoginPage}/>
             <Route path="/home" component={HomePage}/>
         </Router>
     ),
