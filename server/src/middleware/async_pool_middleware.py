8941451e274184c387cb131307499db8c2736a79
Calvin Tran <calvintran1478@protonmail.com>
2025-11-06 17:21:36 -0500

    Replace required database environment variables with a single connection string and remove pgvector extension

--- server/src/middleware/async_pool_middleware.py
+++ server/src/middleware/async_pool_middleware.py
@@ -1,23 +1,14 @@
 import os
 import asyncpg
 import asyncio
-from pgvector.asyncpg import register_vector
 
 class AsyncPoolMiddleware:
     def __init__(self):
         self.pool = None
 
-    async def initialize_vector_db(self, conn):
-        await register_vector(conn)
-
     async def process_startup(self, scope, event):
-        # Enable pgvector extension
-        conn = await asyncpg.connect(database=os.getenv("DB_NAME"), user=os.getenv("DB_USER"))
-        await conn.execute("CREATE EXTENSION IF NOT EXISTS vector")
-        await conn.close()
-
         # Create connection pool
-        self.pool = await asyncpg.create_pool(database=os.getenv("DB_NAME"), user=os.getenv("DB_USER"), init=self.initialize_vector_db)
+        self.pool = await asyncpg.create_pool(os.getenv("DB_CONNECTION"))
 
         # Initialize database tables if needed
         async with self.pool.acquire() as conn:
@@ -28,18 +19,8 @@ class AsyncPoolMiddleware:
                     email VARCHAR UNIQUE NOT NULL,
                     password VARCHAR NOT NULL,
                     first_name VARCHAR NOT NULL,
-                    last_name VARCHAR NOT NULL
-                );
-                """)
-
-            # Create resume table
-            await conn.execute("""
-                CREATE TABLE IF NOT EXISTS resumes(
-                    user_id UUID PRIMARY KEY,
-                    embedding vector(3),
-                    CONSTRAINT resumes_user_id_fkey FOREIGN KEY(user_id) REFERENCES users(user_id)
-                        ON DELETE CASCADE
-                        ON UPDATE CASCADE
+                    last_name VARCHAR NOT NULL,
+                    concentration VARCHAR
                 );
                 """)
 
