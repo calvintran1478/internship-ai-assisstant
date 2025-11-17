89be3ddd0b448aa21acf245b37ee88b09c4acce0
Calvin Tran <calvintran1478@protonmail.com>
2025-11-17 03:44:02 -0500

    Check that the given chat id path parameter is a valid uuid

--- server/src/repositories/chat_repository.py
+++ server/src/repositories/chat_repository.py
@@ -1,11 +1,8 @@
 from uuid import uuid4
 
 async def exists(conn, user_id, chat_id) -> bool:
-    try:
-        statement = await conn.prepare("SELECT EXISTS(SELECT 1 FROM chat_messages WHERE user_id=$1 AND chat_id=$2)")
-        return await statement.fetchval(user_id, chat_id)
-    except:
-        False
+    statement = await conn.prepare("SELECT EXISTS(SELECT 1 FROM chat_messages WHERE user_id=$1 AND chat_id=$2)")
+    return await statement.fetchval(user_id, chat_id)
 
 async def create(conn, user_id, chat_id, message) -> None:
     statement = await conn.prepare("INSERT INTO chat_messages (user_id, chat_id, chat_message, message_number) VALUES ($1, $2, $3, (SELECT COUNT(*) FROM chat_messages WHERE user_id=$4 AND chat_id=$5) + 1)")
